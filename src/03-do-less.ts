7fbfea3634849713715f423d90d5564c49ca3988
Ricardo Martínez <ricardo.martinez.monje@gmail.com>
2024-09-17 19:40:05 +0200

    Add do less topic

---
+++ src/03-do-less.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**
+ * DO LESS
+ */
+
+/**
+ * Use Type Assertions
+ *
+ * Sometimes you will have information about the type of a value that TypeScript
+ * can’t know about.
+ */
+const myCanvas = document.getElementById('main_canvas') as HTMLCanvasElement;
+
+// -- Preparation
+declare const expr: any;
+type T = { a: 1; b: 2; c: 3 };
+// -- End of preparation
+
+const a = expr as any as T;
