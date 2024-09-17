ce7f131fb85ac19b39356b99c70a473ea3dd4f39
Ricardo Martínez <ricardo.martinez.monje@gmail.com>
2024-09-17 19:42:27 +0200

    Add extending types and interfaces topic

---
+++ src/05-extending-types-and-interfaces.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**
+ * Extending Types And Interfaces
+ */
+
+// Interface extends
+interface Colorful {
+  color: string;
+}
+
+interface Circle {
+  radius: number;
+}
+
+interface ColorfulCircle extends Colorful, Circle {}
+
+// Intersection types
+type WithColor = {
+  color: string;
+};
+
+type WithCircle = {
+  radius: number;
+};
+
+type WithColoredCircle = WithColor & WithCircle;
+
+type AnotherColorfulCircle = Colorful & Circle;
+
+const cc1: ColorfulCircle = {
+  color: '#00ff0f',
+  radius: 42,
+};
+
+const cc2: WithColoredCircle = {
+  color: '#00ff0f',
+  radius: 42,
+};
+
+const cc3: AnotherColorfulCircle = {
+  color: '#00ff0f',
+  radius: 42,
+};
